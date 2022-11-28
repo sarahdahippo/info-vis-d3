3f27891d7866218a34b2cae6515a38d59a2a84ab
sarahdahippo <sarahcheah22@gmail.com>
2022-11-27 23:16:02 -0500

    Load data

---
+++ main.js
@@ -0,0 +1,22 @@
+// get the data
+d3.dsv(",", "transportation_fatalities.csv", function (d) {
+    console.log(d)
+    return {
+        year: +d.Year,
+        population: +d.Population,
+        car_occupants: +d.Car_Occupant,
+        pedestrians: +d.Pedestrian,
+        motocyclists: +d.Motorcycle,
+        bicyclists: +d.Bicycle,
+        truck_occupants: +d.Trucks,
+        total: +d.Total,
+        car_occupants_per_100k: +d.Car_Per_100K,
+        pedestrians_per_100k: +d.Ped_Per_100K,
+        motocyclists_per_100k: +d.Motorcycle_Per_100K,
+        bicyclists_per_100k: +d.Bicycle_Per_100K,
+        truck_occupants_per_100k: +d.Trucks_Per_100K,
+        total_per_100k: +d.Total_Per_100K
+    };
+}).then(function (data) {
+    console.log(data);
+})
