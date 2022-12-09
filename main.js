b490b476fc56ece8adb9b095cbb95e570dab9cb6
sarahdahippo <sarahcheah22@gmail.com>
2022-12-09 15:34:32 +0800

    Add axes, title

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
+// define the dimensions and margins for the graph
+const margins = { top: 20, right: 20, bottom: 20, left: 20 };
+const outerWidth = 900;
+const outerHeight = 500;
+const width = outerWidth - margins.left - margins.right;
+const height = outerHeight - margins.top - margins.bottom;
+const titleHeight = 20;
+
+// make the plot
+var barGraph = d3.select("body").selectAll("#bar-graph");
+barGraph.attr("width", outerWidth)
+    .attr("height", outerHeight)
+    .attr("transform", `translate(${margins.left}, ${margins.top})`);
+  
+// make the chart title
+barGraph.append("text")
+    .attr("id", "bar-graph-title")
+    .attr("x", outerWidth / 2)
+    .attr("y", 20)
+    .attr("text-anchor", "middle")
+    .text("Transportation Fatalities by Year");
+
+// make a container for the bars and axes
+var barGraphContainer = barGraph.append("g")
+    .attr("id", "bar-graph-container")
+    .attr("transform", `translate(${margins.left}, ${titleHeight})`);
+
+// initialize the bars
+var bars = barGraphContainer.append("g")
+    .attr("id", "bars");
+
+// make the axes
+barGraphContainer.append("g")
+    .attr("id", "x-axis-bars")
+    .attr("transform", `translate(0, ${height})`);
+barGraphContainer.append("text")
+    .attr("id", "x-axis-bars-label")
+    .attr("x", outerWidth / 2)
+    .attr("y", height + 10)
+    .attr("text-anchor", "middle")
+    .text("Year");
+barGraphContainer.append("g")
+    .attr("id", "y-axis-bars");
+barGraphContainer.append("text")
+    .attr("id", "y-axis-bars-label")
+    .attr("x", -(outerHeight / 2))
+    .attr("transform", "rotate(-90)")
+    .attr("text-anchor", "middle")
+    .text("Number of Deaths");
+
+// creates a bootstrap-slider element
+$("#yearSlider").slider({
+    tooltip: 'always',
+    tooltip_position:'bottom'
+});
+
 // get the data
 d3.dsv(",", "transportation_fatalities.csv", function (d) {
     return {
@@ -18,4 +74,6 @@ d3.dsv(",", "transportation_fatalities.csv", function (d) {
     };
 }).then(function (data) {
     console.log(data);
+
+    // draw the bars
 })
