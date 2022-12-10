14387f31168c261f0df8117669950b3f9375f7c8
mtan62 <mtan29@gatech.edu>
2022-12-10 13:19:51 -0800

    change view to area & line, add filtering

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 /** variables **/
 var dataset;
-var absolute_selected = true;
-var selected_types;
+var area_selected = true;
+
 var types = ["car_occupants", "pedestrians", "motorcyclists", "bicyclists", "truck_occupants"];
-var types_per_100k = ["car_occupants_per_100k", "pedestrians_per_100k", "motorcyclists_per_100k", "bicyclists_per_100k", "truck_occupants_per_100k"];
+var selected_types = types;
 
 var show_total = false;
 
@@ -32,32 +32,30 @@ var view = d3.select("#view")
 
 view.append("input")
     .attr("class", "radio-buttons")
-    .attr("id", "absolute")
+    .attr("id", "area")
     .attr("type", "radio")
     .attr("name", "view")
-    .attr("value", "Absolute Values")
+    .attr("value", "Area")
     .attr("checked", "true")
     .on("click", function() {
-        absolute_selected = true;
-        selected_types = types;
-        updateChart();
+        area_selected = true;
+        updateChart("Area");
         return;
     })
-view.append("label").attr("for", "absolute").text("Absolute Value")
+view.append("label").attr("for", "area").text("Area")
 
 view.append("input")
     .attr("class", "radio-buttons")
-    .attr("id", "per100k")
+    .attr("id", "line")
     .attr("type", "radio")
     .attr("name", "view")
-    .attr("value", "Per 100k")
+    .attr("value", "Line")
     .on("click", function() {
-        absolute_selected = false;
-        selected_types = types_per_100k;
-        updateChart();
+        area_selected = false;
+        updateChart("Line");
         return;
     })
-view.append("label").attr("for", "per100k").text("Per 100k")
+view.append("label").attr("for", "line").text("Line")
 
 // toggle show total checkbox
 var show = d3.select("#show").select("#total-checkbox")
@@ -68,7 +66,8 @@ var show = d3.select("#show").select("#total-checkbox")
         return;
     })
 
-var filter = d3.select("#filter")
+var filter = d3.select("#filter.checkbox")
+// on change of any element in name group, call update chart
 
 
 
@@ -79,7 +78,7 @@ function initAxes() {
         .range([0, graph_width]);
     yScale = d3.scaleLinear()
         .domain([0,d3.max(dataset, function(d) {
-            return absolute_selected ? d.total : d.total_per_100k
+            return area_selected ? d.total : d.total_per_100k
         })])
         .range([graph_height, 0]);
 
@@ -102,33 +101,53 @@ function initAxes() {
 }
 
 /** update chart **/
-function updateChart() {
-    selected_types = absolute_selected ? types : types_per_100k;
+function updateChart(chart_view) {
+    // get selected types from filter
+    selected_types = [];
+    for (i = 0; i < types.length; i++) {
+        var element = document.getElementById(types[i]);
+        if (element.checked) {
+            selected_types.append(types[i]);
+        }
+    }
 
-    // groups & reformat data
-    var stacked_data = d3.stack()
-        .keys(selected_types)
-        (dataset)
+    // show chart view
+    if (chart_view == "Area") {
+        // groups & reformat data
+        var stacked_data = d3.stack()
+            .keys(selected_types)
+            (dataset)
 
-    console.log(stacked_data);
+        console.log(stacked_data);
 
-    // colors
-    var color = d3.scaleOrdinal()
-        .domain(selected_types)
-        .range(['#9abbe6','#c2554f','#a0deb7','#eba57a','#ae85c9'])
+        // colors
+        var color = d3.scaleOrdinal()
+            .domain(selected_types)
+            .range(['#9abbe6','#c2554f','#a0deb7','#eba57a','#ae85c9'])
 
-    // add data
-    svg.selectAll(".layers")
-        .data(stacked_data)
-        .enter()
-        .append("path")
-        .attr("class", "layers")
-        .attr("fill", function(d) { return color(d.key); })
-        .attr("d", d3.area()
-            .x(function(d) { return xScale(d.data.year)})
-            .y0(function(d) { return yScale(d[0])})
-            .y1(function(d) { return yScale(d[1])})
-        )
+        // add data
+        var layers = svg.selectAll(".layers")
+            .data(stacked_data);
+
+        var layers_enter = layers.enter()
+            .append("path")
+            .attr("class", "layers")
+
+        layers_enter.merge(layers)
+            .attr("fill", function(d) { return color(d.key); })
+            .attr("d", d3.area()
+                .x(function(d) { return xScale(d.data.year)})
+                .y0(function(d) { return yScale(d[0])})
+                .y1(function(d) { return yScale(d[1])})
+            )
+
+        layers.exit().remove();
+    } else { // line view
+
+
+    }
+}
+
 
     // show total line
     if (show_total) {
@@ -140,21 +159,21 @@ function updateChart() {
           .attr("stroke-width", 2.0)
           .attr("d", d3.line()
             .x(function(d) { return xScale(d.year) })
-            .y(function(d) { return yScale(absolute_selected ? d.total : d.total_per_100k) })
+            .y(function(d) { return yScale(area_selected ? d.total : d.total_per_100k) })
           )
     } else {
         d3.select(".line").remove();
     }
 
 
-    // update y axis
-    yScale = d3.scaleLinear()
-         .domain([0,d3.max(dataset, function(d) {
-             return absolute_selected ? d.total : d.total_per_100k
-         })])
-         .range([graph_height, 0]);
-    d3.select("#y-axis").call(d3.axisLeft(yScale));
-}
+//    // update y axis
+//    yScale = d3.scaleLinear()
+//         .domain([0,d3.max(dataset, function(d) {
+//             return area_selected ? d.total : d.total_per_100k
+//         })])
+//         .range([graph_height, 0]);
+//    d3.select("#y-axis").call(d3.axisLeft(yScale));
+//}
 
 // get the data
 d3.dsv(",", "transportation_fatalities.csv", function (d) {
